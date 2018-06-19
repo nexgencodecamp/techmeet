afd10eff94a4f329923117c246cc15dc55e10d6c
Connellkai <connellkai@gmail.com>
2018-06-19 15:02:27 +1000

    Add files via upload

---
+++ Ottobot for students!/Coding/Examples/Otto-walk-ultrasonic/Otto-walk-ultrasonic.ino
@@ -0,0 +1,67 @@
+/*
+* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+*
+* Crated by Dejan Nedelkovski,
+* www.HowToMechatronics.com
+*
+*/
+
+#include <Servo.h>
+#include <EEPROM.h>
+#include <Otto.h>
+#include <Oscillator.h>
+#include "MaxMatrix.h"
+#include <US.h>
+#include <BatReader.h>
+Otto Otto;  //This is Otto!
+
+
+#define PIN_YL 2 //servo[0]
+#define PIN_YR 3 //servo[1]
+#define PIN_RL 4 //servo[2]
+#define PIN_RR 5 //servo[3]
+// defines pins numbers
+const int outPin = 2;
+const int trigPin = 8;
+const int echoPin = 9;
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+
+  Otto.init(PIN_YL, PIN_YR, PIN_RL, PIN_RR, true);
+  Otto.home();
+  pinMode(outPin, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  distance= duration*0.034/2;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  if(distance > 20){
+    Otto.walk(1, 1000, 1);
+  }
+  else{
+    Otto.walk(1, 1000, -1);
+  }
+}
